8b8140d55b3dcf2fb65579e3f3caa51378561df8
Malik Umar <malikumar9808@gmail.com>
2024-04-18 23:18:33 +0530

    Made some changes to design

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -11,15 +11,17 @@ function SidebarMenu() {
     <div className="flex items-center justify-between md:hidden  ">
       <div className="logo-container mb-4 md:ml-10 flex justify-between absolute  right-2/3 items-center">
         <button id="menu-toggle" className="md:hidden ml-1" onClick={toggleMenu}>
-          {/* Menu Icon for mobile */}
+          
           <svg className="h-6 w-6  text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
-        <a href="#" className=" md:block">
-          {/* Logo */}
-          <img className="h-20 w-30 bg-[#F2D184]" src="https://logowik.com/content/uploads/images/dribbble.jpg" alt="Logo"></img>
-        </a>
+        <div>
+        <img className="h-20 w-30 bg-[#F2D184]" src="https://logowik.com/content/uploads/images/dribbble.jpg" alt="Logo"></img>
+        </div>
+         
+         
+        
       </div>
       <ul id="menu" className={`md:flex flex-col bg-[#ccc] w-screen z-10 ml-3 h-44  gap-2 mt-2 md:mt-7 mr-10 font-semibold ${showMenu ? '' : 'hidden'}`}>
         <li className='mb-3 '><a href="#">Inspiration</a></li>
